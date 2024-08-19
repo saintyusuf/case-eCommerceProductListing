3772011d26d0bec5c1cbf310c537f6aa85a6a39a
saintyusuf <yusuf1akbaba@gmail.com>
2024-08-19 23:52:10 +0300

    FIX: cart page responsive

--- src/pages/cart.page.tsx
+++ src/pages/cart.page.tsx
@@ -39,15 +39,19 @@ const CartPage = () => {
         <title>Cart</title>
       </Helmet>
       <Box display="flex" flexDir={{mobile: "column", desktop: "row"}} h="100%">
-        <Box display="flex" flexDir="column" w={{mobile: "100%", desktop: "75%"}} p="10px">
+        <Box display="flex" flexDir="column" w={{mobile: "100%", desktop: "75%"}} p={{mobile: "5px", desktop: "10px"}}>
           <CartRows cartItemsFullData={cartItemsFullData} rowProps={{h: "100px"}}/>
         </Box>
-        <Box pos="sticky" top="110px" w={{mobile: "100%", desktop: "25%"}} h="50%" border="1px solid var(--borderColor)" borderRadius="10px" p="10px">
+        <Box pos="sticky" top="110px" w={{mobile: "100%", desktop: "25%"}} h="fit-content" border="1px solid var(--borderColor)" borderRadius="10px" p="10px">
           <Box display="flex" mb="10px">
-            <Text fontSize="20px" mr="auto">Total:</Text>
-            <Text fontSize="20px" fontWeight="600">${cartTotalPrice}</Text>
+            <Text fontSize={{mobile: "16px", desktop: "20px"}} mr="auto">Total:</Text>
+            <Text fontSize={{mobile: "16px", desktop: "20px"}} fontWeight="600">${cartTotalPrice}</Text>
           </Box>
-          <Checkbox mb="10px" isChecked={isAgreed} onChange={(e)=>setIsAgreed(e.target.checked)}>I accept all agreements</Checkbox>
+          <Box display="flex" mb="10px">
+            <Checkbox isChecked={isAgreed} onChange={(e)=>setIsAgreed(e.target.checked)}>
+              <Text fontSize={{mobile: "14px", desktop: "16px"}}>I accept all agreements</Text>
+            </Checkbox>
+          </Box>
           <ButtonComponent onClick={()=>navigate("/checkout")} w="100%" isDisabled={cartItemsFullData.length === 0 || !isAgreed ? true : false}>Checkout</ButtonComponent>
         </Box>
       </Box>
