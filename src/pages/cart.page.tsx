4ec74ea1ea2a67059b6bdf91ddd2b9fed52633cb
saintyusuf <yusuf1akbaba@gmail.com>
2024-08-19 12:19:29 +0300

    NEW: checkout if at least one product in cart and agreements accepted

--- src/pages/cart.page.tsx
+++ src/pages/cart.page.tsx
@@ -1,4 +1,4 @@
-import { Box, Text } from "@chakra-ui/react"
+import { Box, Checkbox, Text } from "@chakra-ui/react"
 import { useEffect, useState } from "react"
 import { CartFullDataType } from "../types/cart.type"
 import User from "../classes/user.class"
@@ -17,11 +17,14 @@ const CartPage = () => {
   const userStates = useSelector((state:RootState)=>state.user)
   const [cartItemsFullData, setCartItemsFullData] = useState<CartFullDataType[]>([])
   const [cartTotalPrice, setCartTotalPrice] = useState<string>("")
+  const [isAgreed, setIsAgreed] = useState<boolean>(false)
 
   useEffect(()=>{
     user.getCartItemsFullData().then((data) => setCartItemsFullData(data))
     user.getCartTotalPrice().then((data) => setCartTotalPrice(data))
   },[userStates.cart])
+
+  console.log(cartItemsFullData.length)
   
   return (
     <>
@@ -33,11 +36,12 @@ const CartPage = () => {
           <CartRows cartItemsFullData={cartItemsFullData} rowProps={{h: "100px"}}/>
         </Box>
         <Box pos="sticky" top="110px" w="25%" h="50%" border="1px solid var(--borderColor)" borderRadius="10px" p="10px">
-          <Box display="flex">
-            <Text fontSize="20px" mr="auto" mb="10px">Total:</Text>
+          <Box display="flex" mb="10px">
+            <Text fontSize="20px" mr="auto">Total:</Text>
             <Text fontSize="20px" fontWeight="600">${cartTotalPrice}</Text>
           </Box>
-          <ButtonComponent onClick={()=>navigate("/checkout")} w="100%">Checkout</ButtonComponent>
+          <Checkbox mb="10px" isChecked={isAgreed} onChange={(e)=>setIsAgreed(e.target.checked)}>I accept all agreements</Checkbox>
+          <ButtonComponent onClick={()=>navigate("/checkout")} w="100%" isDisabled={cartItemsFullData.length === 0 || !isAgreed ? true : false}>Checkout</ButtonComponent>
         </Box>
       </Box>
     </>
